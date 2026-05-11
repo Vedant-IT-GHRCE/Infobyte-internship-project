6853e024214777aa3ced7dec0c07bad832d3d6c0
Vedant-IT-GHRCE <vedantraut138@gmail.com>
2026-05-11 14:02:45 +0530

    Update Bank.java

--- Bank.java
+++ Bank.java
@@ -9,9 +9,9 @@ public class Bank {
 
     public Bank() {
         // Pre-loaded demo accounts
-        accounts.put("1001", new Account("1001", "1234", "Ravi Sharma",  50000.00));
-        accounts.put("1002", new Account("1002", "5678", "Priya Mehta",  25000.00));
-        accounts.put("1003", new Account("1003", "9999", "Amit Kumar",  100000.00));
+        accounts.put("0121", new Account("0121", "2006", "Vedant Raut",  25000.00));
+        accounts.put("0122", new Account("0122", "1985", "Hemraj Raut",  100000.00));
+        accounts.put("0123", new Account("0123", "1990", "Sushma Raut",  50000.00));
     }
 
     public Account findAccount(String accountNumber) {
