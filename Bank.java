5f90e5d6273a68fbcbbc47ee51ff03521a50b130
Vedant Raut <vedantraut138@gmail.com>
2026-05-12 12:51:46 +0530

    Update Bank.java

--- Bank.java
+++ Bank.java
@@ -9,9 +9,9 @@ public class Bank {
 
     public Bank() {
         // Pre-loaded demo accounts
-        accounts.put("0121", new Account("0121", "2006", "Vedant Raut",  25000.00));
-        accounts.put("0122", new Account("0122", "1985", "Hemraj Raut",  100000.00));
-        accounts.put("0123", new Account("0123", "1990", "Sushma Raut",  50000.00));
+        accounts.put("0121", new Account("1001", "2006", "Vedant Raut",  25000.00));
+        accounts.put("0122", new Account("1002", "1985", "Hemraj Raut",  100000.00));
+        accounts.put("0123", new Account("1003", "1990", "Sushma Raut",  50000.00));
     }
 
     public Account findAccount(String accountNumber) {
